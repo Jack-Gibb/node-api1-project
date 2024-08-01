4353ff3a3016d3237a4ae245e684de1457afa7d0
Jack Gibb <ogrewithissues@yahoo.com>
2024-08-01 17:20:04 -0600

    Finished

--- api/server.js
+++ api/server.js
@@ -4,14 +4,38 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
-server.put('/api/users/')
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const possibleUser = await User.findById(req.params.id)
+        if (!possibleUser) {
+            res.status(404).json({
+                message: "The user with the specified ID does not exist",
+            })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({
+                    message: "Please provide name and bio for the user",
+                })
+            } else {
+               const updatedUser = await User.update(req.params.id, req.body)
+               res.status(200).json(updatedUser)
+            }
+        }
+    } catch (err) {
+        res.status(500).json({
+            message:'error updating user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
 
 server.delete('/api/users/:id', async (req, res) => {
 try {
     const possibleUser = await User.findById(req.params.id)
     if (!possibleUser) {
         res.status(404).json({
-            message: "The user with the specified ID does not exist"
+            message: "The user with the specified ID does not exist",
         })
     } else {
         const deletedUser = await User.remove(possibleUser.id)
@@ -19,19 +43,18 @@ try {
     }
  }  catch (err) {
         res.status(500).json({
-            message:'error getting user',
+            message:'error updating user',
             err: err.message,
             stack: err.stack,
         })
     }
 })
 
-
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
         res.status(400).json({
-            message: "Please provide name and bio for the user"
+            message: "Please provide name and bio for the user",
         })
     } else {
          User.insert(user)
@@ -83,7 +106,7 @@ server.get('/api/users/:id', (req, res) => {
 
 server.use('*', (req, res) => {
     res.status(404).json({
-        message: 'not found'
+        message: 'not found',
     })
 })
 
