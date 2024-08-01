7e8ded3fbec9f3cd4e0b70812c01e5aa09672aae
Jack Gibb <ogrewithissues@yahoo.com>
2024-08-01 16:53:53 -0600

    WIP

--- api/server.js
+++ api/server.js
@@ -2,6 +2,51 @@
 const express = require('express')
 const User = require('./users/model')
 const server = express()
+server.use(express.json())
+
+server.put('/api/users/')
+
+server.delete('/api/users/:id', async (req, res) => {
+try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+        res.status(404).json({
+            message: "The user with the specified ID does not exist"
+        })
+    } else {
+        const deletedUser = await User.remove(possibleUser.id)
+        res.status(200).json(deletedUser)
+    }
+ }  catch (err) {
+        res.status(500).json({
+            message:'error getting user',
+            err: err.message,
+            stack: err.stack,
+        })
+    }
+})
+
+
+server.post('/api/users', (req, res) => {
+    const user = req.body;
+    if (!user.name || !user.bio) {
+        res.status(400).json({
+            message: "Please provide name and bio for the user"
+        })
+    } else {
+         User.insert(user)
+         .then(createdUser => {
+            res.status(201).json(createdUser)
+         })
+         .catch(err => {
+            res.status(500).json({
+                message:'error creating user',
+                err: err.message,
+                stack: err.stack,
+            })
+        })
+    }
+})
 
 server.get('/api/users', (req, res) => {
     User.find()
@@ -10,7 +55,7 @@ server.get('/api/users', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                message:'error geting users',
+                message:'error getting users',
                 err: err.message,
                 stack: err.stack,
             })
@@ -29,7 +74,7 @@ server.get('/api/users/:id', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({
-                message:'error geting user',
+                message:'error getting user',
                 err: err.message,
                 stack: err.stack,
             })
